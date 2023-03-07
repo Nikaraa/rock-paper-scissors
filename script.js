eb71ef886e097999c2d33ca74b3089bc16e32e91
nikaraa <giuseppemalara00001@gmail.com>
2023-03-07 19:00:56 +0100

    Html code CSS and revisit javascript

--- script.js
+++ script.js
@@ -1,63 +1,80 @@
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
     if(computerChoice==0){
-        return "rock";
+        cpu= "rock";
     }
     else if(computerChoice==1){
-        return "paper";
+        cpu= "paper";
     }
     else{
-        return "scissors";
+        cpu= "scissors";
+    }
+    return cpu.toLowerCase();
+}
+
+function winner(){
+    cpu=cpu.toLowerCase();
+    player=player.toLowerCase();
+    if((cpu == "rock"&&player=="paper")||(cpu == "paper"&&player=="scissors")||(cpu == "scissors"&&player=="rock")){
+        return "You win!";
+    }
+    else if(player == cpu){
+        return "Draw!";
+    }
+    else{
+        return "Computer wins!"; 
     }
 }
 
-function winner(user_choice, computer_choise){
-    if(user_choice===computer_choise){
-        return "draw";
-    }
-    else{
-        if((user_choice=="rock"&& computer_choise=="scissors")||(user_choice=="paper"&&computer_choise=="rock")||(user_choice=="scissors"&&computer_choise=="paper")){
-            return "won";
-        }
-        else{
-            return "lost";
-        }
-    }
-}
+const userButtons= document.querySelectorAll('#user-buttons');
 
-function game(){
-    let userScore=0;
-    let cpuScore=0;
+const playerText= document.querySelector('.playerText');
+const cpuText= document.querySelector('.cpuText');
+const resultText= document.querySelector('.res');
 
-    for(let i=0;i<10;i++){
-        let userChoice = prompt("Please choose between rock, scissors or paper: ").toLowerCase();
-        let computerChoice = getComputerChoice();
-        let x=winner(userChoice, computerChoice);
-        if(x=="draw"){
-            console.log("User choice is "+userChoice+" and cpu choice is: "+computerChoice+". It's a tie!");
-            i--; 
-        }
-        else if(x=="won"){
-            userScore++;
+let player;
+let cpu;
+let result;
 
-            console.log("User choice is "+userChoice+" and cpu choice is: "+computerChoice+". You got a point!");
-        }
-            
-        else if(x="lost"){
-            cpuScore++;
-            console.log("User choice is "+userChoice+" and cpu choice is: "+computerChoice+". The cpu got a point!");
-        }
-        if(userScore==5||computerChoice==5){
-            break;
-        }
-    } 
-    if(cpuScore=5&&userScore<5){
-        return console.log("You lost the game! Try again!");
-    }
-    else{
-        return console.log("You won the game! Congratulations!");
-    }
-}
+userButtons.forEach(button => button.addEventListener('click', () => {
+    player=button.textContent;
+    getComputerChoice();
+    playerText.textContent = `Player: ${player}`;
+    cpuText.textContent = `CPU: ${cpu}`;
+    resultText.textContent = "Result: "+winner();
+}));
 
-let x=game();
-console.log(x);
+
+//function game(){
+//   let userScore=0;
+//     let cpuScore=0;
+//     for(let i=0;i<10;i++){
+//         let userChoice = prompt("Please choose between rock, scissors or paper: ").toLowerCase();
+//         let computerChoice = getComputerChoice();
+//         let x=winner(userChoice, computerChoice);
+//         if(x=="draw"){
+//             console.log("User choice is "+userChoice+" and cpu choice is: "+computerChoice+". It's a tie!");
+//             i--; 
+//         }
+//         else if(x=="won"){
+//             userScore++;
+//             console.log("User choice is "+userChoice+" and cpu choice is: "+computerChoice+". You got a point!");
+//         }
+//         else if(x="lost"){
+//             cpuScore++;
+//             console.log("User choice is "+userChoice+" and cpu choice is: "+computerChoice+". The cpu got a point!");
+//         }
+//         if(userScore==5||computerChoice==5){
+//             break;
+//         }
+//     } 
+//     if(cpuScore=5&&userScore<5){
+//         return console.log("You lost the game! Try again!");
+//     }
+//     else{
+//         return console.log("You won the game! Congratulations!");
+//     }
+// }
+
+// let x=game();
+// console.log(x);
